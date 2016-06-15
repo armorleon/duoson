f2e6129c29ced06c6ab77c7598558fb52f046b48
Wei Yin <wyin33@gatech.edu>
2016-06-14 21:22:22 -0500

    start project

---
+++ src/main/java/weiyin/duoson/App.java
@@ -0,0 +1,13 @@
+package weiyin.duoson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
